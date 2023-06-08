8a78d8abe27a25ee569c7f7d2144c06b2652e6b7
unknown <roumamedj@gmail.com>
2023-06-08 12:02:00 +0100

    mission_2

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sqlalchemy
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, insert
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -12,7 +12,7 @@ engine = create_engine('sqlite:///alexandria.db', echo=True)
 Base = declarative_base()
 Base.metadata.create_all(engine)
 
-#add bokk table
+#add book table
 class Book(Base):
     __tablename__ = "book"
     book_id = Column(Integer, primary_key=True)
@@ -24,6 +24,18 @@ class Book(Base):
     
 Base.metadata.create_all(engine)
 
+#inserting 5 books
+with engine.connect() as conn:
+    result = conn.execute(
+        insert(Book),
+        [
+            {"book_id":521,"author":"Laura Mitchell","title":"The Secret Garden of Dreams","description":"A heartwarming tale about a young girl who discovers a magical garden that brings hope and healing to all who enter.","year":2009},
+            {"book_id":522,"author":"Laura Mitchell","title":"The Secret Garden of Dreams","description":"A heartwarming tale about a young girl who discovers a magical garden that brings hope and healing to all who enter.","year":2009},
+            {"book_id":520,"author":"Laura Mitchell","title":"The Secret Garden of Dreams","description":"A heartwarming tale about a young girl who discovers a magical garden that brings hope and healing to all who enter.","year":2009},
+            {"book_id":50,"author":"Laura Mitchell","title":"The Secret Garden of Dreams","description":"A heartwarming tale about a young girl who discovers a magical garden that brings hope and healing to all who enter.","year":2009},
+            {"book_id":16,"author":"Laura Mitchell","title":"The Secret Garden of Dreams","description":"A heartwarming tale about a young girl who discovers a magical garden that brings hope and healing to all who enter.","year":2009},
+        ],
+    )
+    conn.commit()
 
-#connect to alexandria
-engine.connect()
+
